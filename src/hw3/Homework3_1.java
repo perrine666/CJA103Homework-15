1ac4e77f148b80c9306e768e79550d90006e9426
Pei Yun Luo <luopei789@gmail.com>
2025-10-26 14:21:48 +0800

    Homework3_2 finished

--- src/hw3/Homework3_1.java
+++ src/hw3/Homework3_1.java
@@ -2,7 +2,7 @@ package hw3;
 
 import java.util.Scanner;
 
-public class Homework_1 {
+public class Homework3_1 {
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("請輸入三個整數：");
--- src/hw3/Homework3_2.java
+++ src/hw3/Homework3_2.java
@@ -10,15 +10,35 @@ public class Homework3_2 {
 		System.out.println("開始猜數字吧!");
 		
 		while (true) {
-			Scanner isanswer = new Scanner(System.in);
+			Scanner number = new Scanner(System.in);
 			
-			if(isanswer.nextInt()!=i) {
+			if(number.nextInt()!=i) {
 				System.out.println("猜錯囉！");
 			}else {
 				System.out.println("答對了！答案就是"+i);
 				break;
 			}
 			
+			
+		}
+		
+		//進階題
+		int j = (int)(Math.random()*101);
+		System.out.println("開始猜數字吧！");
+		
+		while (true) {
+			Scanner number1 = new Scanner(System.in);
+			
+			int guessNumber = number1.nextInt();
+			
+			if (guessNumber>j) {
+				System.out.println("大於正確答案");
+			}else if (guessNumber<j) {
+				System.out.println("小於正確答案");
+			}else { 
+				System.out.println("答對了！答案就是"+j);
+				break;
+			}
 			
 		}
 
