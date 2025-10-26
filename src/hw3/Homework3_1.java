21eb4708d33029da3bd7d3d664abae875bb0a46c
Pei Yun Luo <luopei789@gmail.com>
2025-10-26 16:06:34 +0800

    Homework3_2 modified

--- src/hw3/Homework3_1.java
+++ src/hw3/Homework3_1.java
@@ -1,21 +1,29 @@
 package hw3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Homework3_1 {
 	public static void main(String[]args) {
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("請輸入三個整數：");
 		
-		int data1 = sc.nextInt();
-		int data2 = sc.nextInt();
-		int data3 = sc.nextInt();
+		int [] data = new int [3];
 		
-		if (data1==0 && data2==0 && data3==0) {
+		for (int i = 0; i<data.length; i++ ) {
+			data[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(data);
+		
+		if (data[0]==0) {
 			System.out.println("不是三角形");
-		}else if (data1 == data2 && data2 == data3) {
+		}else if (data[0] == data[1] && data[1] == data[2]) {
 			System.out.println("正三角形");
-		}else if((data1==data2) || (data2==data3) || (data1==data3)) {
+		}else if (Math.pow(data[2],2) == (Math.pow(data[0], 2) + Math.pow(data[1], 2)) ) {
+			System.out.println("直角三角形");
+		}else if (data[0]==data[1] || data[1] == data[2] ) {
 			System.out.println("等腰三角形");
 		}else {
 			System.out.println("其他三角形");
